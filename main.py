1900034c0a2d1d0b347e69b28760c7b98f90d20e
Kurowski Marcin <m.kurowski.2@stud.elka.pw.edu.pl>
2022-03-05 01:08:34 +0100

    Basic POST and GET for Pokemon

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import current_user
 from flask_user import roles_accepted
+
+from app import db
+from models import Pokemon, User
+from schemas import pokemon_schema, pokemons_schema
 
 main = Blueprint('main', __name__)
 
@@ -14,3 +18,26 @@ def index():  # put application's code here
 @roles_accepted('admin', 'user')
 def profile():
     return render_template('profile.html', name=current_user.username)
+
+
+@main.route('/add_pokemon', methods=['POST'])
+def add_pokemon():
+    name = request.json['name']
+    owner = request.json['owner']
+
+    if User.query.filter_by(username=owner).first():
+        new_pokemon = Pokemon(name=name, owner=owner)
+        db.session.add(new_pokemon)
+        db.session.commit()
+
+        return pokemon_schema.jsonify(new_pokemon)
+
+    return "No such user, try again"
+
+
+@main.route('/get_pokemon', methods=['GET'])
+def get_all_pokemon():
+    all_pokemon = Pokemon.query.all()
+    result = pokemons_schema.dump(all_pokemon)
+
+    return jsonify(result)
--- schemas.py
+++ schemas.py
@@ -7,14 +7,19 @@ class UserSchema(ma.SQLAlchemySchema):
         model = models.User
 
     id = ma.auto_field()
-    name = ma.auto_field()
+    username = ma.auto_field()
     password = ma.auto_field()
 
 
 class PokemonSchema(ma.SQLAlchemySchema):
     class Meta:
         model = models.Pokemon
+        include_fk = True
 
     id = ma.auto_field()
     name = ma.auto_field()
     owner = ma.auto_field()
+
+
+pokemon_schema = PokemonSchema()
+pokemons_schema = PokemonSchema(many=True)
